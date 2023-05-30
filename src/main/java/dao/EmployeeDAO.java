1ac1d71d977e1a47fbfa2b67856dc1eddf1fbb5a
alkor63 <alkor@medi-spa.ru>
2023-05-31 02:03:56 +0300

    3 commit

--- src/main/java/dao/EmployeeDAO.java
+++ src/main/java/dao/EmployeeDAO.java
@@ -1,4 +1,16 @@
 package dao;
 
+import model.Employee;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface EmployeeDAO {
+    void create(Employee employee) throws SQLException;
+
+    Employee findById(Integer id);
+
+    List<Employee> findAll();
+
+    void deleteById(Integer id);
 }
