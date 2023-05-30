11c723fb937e330abab75e4528fa2022eb04bb5d
alkor63 <alkor@medi-spa.ru>
2023-05-31 00:32:14 +0300

    2nd commit

---
+++ src/main/java/model/City.java
@@ -0,0 +1,8 @@
+package model;
+import lombok.Data;
+
+@Data
+public class City {
+    private Integer city_id;
+    private String city_name;
+}
---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,12 @@
+package model;
+import lombok.Data;
+
+@Data
+public class Employee {
+private Integer id;
+private String first_name;
+private String last_name;
+private String gender;
+private Integer age;
+private Integer city_id;
+}
