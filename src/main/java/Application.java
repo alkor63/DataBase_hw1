6caa937e1d2610d5ad64928b1aa84853d026d96a
alkor63 <alkor@medi-spa.ru>
2023-06-05 15:58:27 +0300

    Сформировал слой DAO для City

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -10,24 +10,24 @@ public class Application {
     public static void main(String[] args) throws SQLException {
         // Создаем объект класса ДАО
         EmployeeDAO employeeDAO = new EmployeeDAOImpl();
-        Employee employee1 = new Employee("Taras", "Bulba", "male", 62, 4);
-        // Создаем объект
-        employeeDAO.create(employee1);
-        // Получаем объект по id
+//        Employee employee1 = new Employee("Taras", "Bulba", "male", 62, 4);
+//        // Создаем объект
+//        employeeDAO.create(employee1);
+//        // Получаем объект по id
         System.out.println(employeeDAO.findById(16));
         // Получаем полный список объектов
         List<Employee> list = employeeDAO.findAll();
         for (Employee employee : list) {
             System.out.println(employee);
         }
-        Employee employee2 = new Employee(18,"Ruslan", "Ludmilin", "male", 33, 4);
+//        Employee employee2 = new Employee(18,"Ruslan", "Ludmilin", "male", 33, 4);
         // Изменяем объект
-        employeeDAO.update(employee2);
-        System.out.println(employee2);
+//        employeeDAO.update(employee2);
+//        System.out.println(employee2);
 
-        employee2 = employeeDAO.findById(19);
+//        employee2 = employeeDAO.findById(19);
         // Удаляем объект
-        employeeDAO.deleteEmployee(employee2);
+//        employeeDAO.deleteEmployee(employee2);
         //смотрим что получилось
         list = employeeDAO.findAll();
         for (Employee employee : list) {
---
+++ src/main/java/dao/CityDAOImpl.java
@@ -0,0 +1,48 @@
+package dao;
+
+import model.City;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import utils.HibernateSessionFactoryUtil;
+
+import java.util.List;
+
+public class CityDAOImpl implements CityDAO {
+    @Override
+    public void createCity(City city) {
+        try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();) {
+            Transaction transaction = session.beginTransaction();
+            session.save(city);
+            transaction.commit();
+        }
+    }
+
+    @Override
+    public City findByCityId(Integer id) {
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(City.class, id);
+    }
+
+    @Override
+    public List<City> findAllCity() {
+        return (List<City>) HibernateSessionFactoryUtil
+                .getSessionFactory().openSession().createQuery("FROM City ").list();
+    }
+
+    @Override
+    public void updateCity(City city) {
+        try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.update(city);
+            transaction.commit();
+        }
+    }
+
+    @Override
+    public void deleteCity(City city) {
+        try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.delete(city);
+            transaction.commit();
+        }
+    }
+}
